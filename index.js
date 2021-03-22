36057658de74d20d521939b1faa33fb39ab213ce
tanoto-the-explorer <michelletanotoo@gmail.com>
2021-03-22 12:18:07 +1100

    add index.ejs

--- index.js
+++ index.js
@@ -2,16 +2,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-var router = express.Router();
 
 // bot routes
 const webhookRoutes = require('./routes/webhook');
 
-app.use(bodyParser.json())
 app.listen(process.env.PORT || 3000, () => console.log('Webhook is listening'));
+app.use(bodyParser.json())
+app.set('view engine', 'ejs');
 
-router.get('/', (req, res, next) => {  
-  res.sendFile('index.html')
+app.get('/', (req, res, next) => {  
+  res.render('index')
 });  
 
 app.use('/webhook', webhookRoutes);
