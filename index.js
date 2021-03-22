48f8d85d9f0b839add87e4462f27c5e61606a701
tanoto-the-explorer <michelletanotoo@gmail.com>
2021-03-22 12:08:53 +1100

    add / route

--- index.js
+++ index.js
@@ -2,11 +2,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+var router = express.Router();
 
 // bot routes
 const webhookRoutes = require('./routes/webhook');
 
 app.use(bodyParser.json())
 app.listen(process.env.PORT || 3000, () => console.log('Webhook is listening'));
+
+router.get('/', (req, res, next) => {  
+  res.sendFile('index.html')
+});  
 
 app.use('/webhook', webhookRoutes);
