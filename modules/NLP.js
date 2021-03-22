672b449368fb68a966f0926894e618bf4f3f8ad0
tanoto-the-explorer <michelletanotoo@gmail.com>
2021-03-22 20:37:51 +1100

    .gitignore misconfi

---
+++ modules/NLP.js
@@ -0,0 +1,17 @@
+const { dockStart } = require('@nlpjs/basic');
+
+const trainNLP = async (message) => {
+    const dock = await dockStart();
+    let nlp = dock.get('nlp')
+    await nlp.train();
+    const response = await nlp.process('en', message);
+    if(response.answers.length > 0){
+      let rand = Math.floor(Math.random() * response.answers.length);
+      return response.answers[rand].answer
+    }
+    return null
+}
+
+module.exports = {
+  trainNLP,
+}
